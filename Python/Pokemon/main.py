cd5289083f313fc4c34dcf210ffe3ec70ae80781
VinukaDeSilva <vinukads@gmail.com>
2022-07-11 07:52:22 +0530

    Attempt to fix errors

--- Python/Pokemon/main.py
+++ Python/Pokemon/main.py
@@ -9,6 +9,7 @@ moves = {
 
 class Character:
     """We create the Character class that we use to represent the player and their opponent"""
+
     def __init__(self, health):
         self.health = health
 
@@ -18,13 +19,14 @@ class Character:
 
 class Player(Character):
     # The player starts with 100 health
-    def __init__(self, health = 100):
+    def __init__(self, health=100):
         super().__init__(health)
-    
+
     # Set an attack function that gives the player 3 moves
     def attack(self, other):
         while True:
-            choice = str.lower(input("What move woulf you like to chose:  basic move, special move, heal"))
+            choice = str.lower(
+                input("What move woulf you like to chose:  basic move, special move, heal: "))
 
             if choice == "basic move" or choice == "special move":
                 damage = int(random.choice(moves[choice]))
@@ -41,7 +43,7 @@ class Player(Character):
 
 class Opponent(Character):
     # The opponent starts with 100 health
-    def __init__(self, health = 100):
+    def __init__(self, health=100):
         super().__init__(health)
 
     def attack(self, other):
@@ -51,14 +53,16 @@ class Opponent(Character):
             Opponent_choice = random.choice(moves_1)
         else:
             Opponent_choice = random.choice(list[moves])
-        
+
         if Opponent_choice == "basic move" or Opponent_choice == "special move":
             damage = int(random.choice(moves[Opponent_choice]))
             other.health -= damage
-            print(f"Opponent attack with {Opponent_choice}. He dealt {damage} damage")
+            print(
+                f"Opponent attack with {Opponent_choice}. He dealt {damage} damage")
         elif Opponent_choice == "heal":
             self.health += random.choice(moves[Opponent_choice])
             print(f"Opponent's health is now {self.health}.")
+
 
 def battle(player, Opponent):
     print("Enemy opponent enters...")
@@ -72,11 +76,12 @@ def battle(player, Opponent):
         if player.health <= 0:
             break
         print(f"Your health is now {player.health}.")
-    
+
     if player.health > 0:
         print("You defeated the opponent!")
     if Opponent.health > 0:
         print("You were defeated by the opponent!")
+
 
 if __name__ == "__main__":
     battle(Player(), Opponent())
